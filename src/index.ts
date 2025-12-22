f3228509197f0b7dda457ce28c3073ba8308b2c7
Yash pawar <yashpawar12122004@gmail.com>
2025-12-23 00:14:27 +0530

    added frontend deployed url

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 
 export const io = new SocketIOServer(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://roadmap-website-frontend-5xdx.vercel.app"],
     credentials: true,
   },
 });
@@ -62,7 +62,7 @@ io.on("connection", (socket) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://roadmap-website-frontend-5xdx.vercel.app"],
     credentials: true,
   })
 );
